5d40fd8d11f69791a67527b848f425def76e3554
Jason Neal <jasonneal1@quickenloans.com>
2021-03-27 18:21:03 -0400

    ng g component --project mfe1 example1

--- projects/mfe1/src/app/app.module.ts
+++ projects/mfe1/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Example1Component } from './example1/example1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Example1Component
   ],
   imports: [
     BrowserModule,
