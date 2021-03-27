b884bbcf0970aa803480ab7e22666144fcd3c75a
Jason Neal <jasonneal1@quickenloans.com>
2021-03-27 18:23:49 -0400

    Update projects/mfe1/src/app/app-routing.module.ts

--- projects/mfe1/src/app/app-routing.module.ts
+++ projects/mfe1/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { Example1Component } from './example1/example1.component';
+import { Example2Component } from './example2/example2.component';
+import { Example3Component } from './example3/example3.component';
+
+const routes: Routes = [
+  { component: Example1Component, path: 'example1' },
+  { component: Example2Component, path: 'example2' },
+  { component: Example3Component, path: 'example3' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
