87639273e3f087fd63dffbbf68b1c6c17bbdbd15
Jason Neal <jasonneal1@quickenloans.com>
2021-03-27 21:23:57 -0400

    ng add ng-micro-frontend --project mfe2 --type micro --port 4220

--- projects/mfe2/src/app/app.module.ts
+++ projects/mfe2/src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ApplicationRef, DoBootstrap, Injector } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { createCustomElement } from '@angular/elements';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,19 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule implements DoBootstrap { 
+  public constructor(
+    private readonly injector: Injector
+  ) { }
+
+  public ngDoBootstrap(appRef: ApplicationRef): void {
+    if (environment.production) {
+      const customElement = createCustomElement(AppComponent, { injector: this.injector });
+      customElements.define('custom-mfe2', customElement);
+    } else {
+      appRef.bootstrap(AppComponent);
+    }
+  }
+
+}
