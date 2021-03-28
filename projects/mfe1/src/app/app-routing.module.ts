3f886cba69471ce4bca0543eb2fc2da5cfd0a1ba
Jason Neal <jasonneal1@quickenloans.com>
2021-03-27 21:25:16 -0400

    Fix mfe1 routes

--- projects/mfe1/src/app/app-routing.module.ts
+++ projects/mfe1/src/app/app-routing.module.ts
@@ -6,12 +6,13 @@ import { Example2Component } from './example2/example2.component';
 import { Example3Component } from './example3/example3.component';
 import { NullComponent } from 'ng-micro-frontend';
 
-const mfeRoutes: Routes = [];
-
-const routes: Routes = [
+const mfeRoutes: Routes = [
   { component: Example1Component, path: 'example1' },
   { component: Example2Component, path: 'example2' },
-  { component: Example3Component, path: 'example3' },
+  { component: Example3Component, path: 'example3' }
+];
+
+const routes: Routes = [
   { children: mfeRoutes, path: 'mfe1' },
   { component: NullComponent, path: '**' }
 ];
